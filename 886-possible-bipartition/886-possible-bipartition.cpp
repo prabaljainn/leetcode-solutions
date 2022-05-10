cc43cc846d62143f56d9511829fb3404860d6270
Prabal Jain <prabaljainn@gmail.com>
2022-05-10 08:15:31 +0530

    Time: 215 ms (79.74%), Space: 72.1 MB (20.52%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.cpp
+++ 886-possible-bipartition/886-possible-bipartition.cpp
@@ -1,3 +1,9 @@
+static const auto ____ = [](){
+	// fast I/O
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr), cout.tie(nullptr);
+    return 0;
+}();
 class Solution {
 public:
     vector<vector<int>>g;
