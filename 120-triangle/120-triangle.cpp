381a77c4850229ad846e8f36c3d2039f5b34c3b7
Prabal Jain <prabaljainn@gmail.com>
2022-06-14 17:15:18 +0530

    Time: 9 ms (50.38%), Space: 9.3 MB (5.40%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,27 +1,34 @@
-class Solution {
-public:
-    int n;
-    bool vis[400][400];
+class Solution
+{
+    public:
+        int n;
+    // bool vis[400][400];
     int dp[400][400];
-    int rec(vector<vector<int>>& triangle, int level, int idx){ //returns the best you can make on level taking index i
-        if(level==n)
+    int rec(vector<vector < int>> &triangle, int level, int idx)
+    
+    {
+       	//returns the best you can make on level taking index idx
+        if (level == n)
             return 0;
-        if(idx >level or idx<0)
+        if (idx > level or idx < 0)
             return 1e9;
-        if(vis[level][idx]==true)
+        if (dp[level][idx] != -1)
             return dp[level][idx];
-        
-        vis[level][idx] = true;
-        
+
+        // vis[level][idx] = true;
+
         int ans = triangle[level][idx];
-        
-        ans = ans+ min( rec(triangle, level+1, idx), rec(triangle, level+1, idx+1));
+
+        ans = ans + min(rec(triangle, level + 1, idx), rec(triangle, level + 1, idx + 1));
         return dp[level][idx] = ans;
-        }
-    int minimumTotal(vector<vector<int>>& triangle) {
-        memset(vis, false, sizeof(vis));
+    }
+    
+    int minimumTotal(vector<vector < int>> &triangle)
+    {
+        memset(dp,-1, sizeof(dp));
         n = triangle.size();
         return rec(triangle, 0, 0);
-        
     }
 };
+
+
