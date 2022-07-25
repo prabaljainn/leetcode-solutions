825a424775e0150e13866b2bf12d27255e061057
Prabal Jain <prabaljainn@gmail.com>
2022-07-25 16:20:12 +0530

    Time: 60 ms (18.00%), Space: 10.9 MB (22.87%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -6,7 +6,7 @@ public:
         int head = -1; int tail = 0;
         //len is head-tail +1;
         int ans =0 ; 
-        set<int> se;
+        unordered_set<int> se;
         while(tail<n){
             // take head upto max you can take
             while((head+1 < n) and (se.find(s[head+1])==se.end()) ){
