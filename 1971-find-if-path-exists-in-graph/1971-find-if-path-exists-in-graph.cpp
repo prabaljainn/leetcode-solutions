b7a79a7c416fc50c28d0f28f91ada064a361e93b
Prabal Jain <prabaljainn@gmail.com>
2022-07-29 14:59:06 +0530

    Time: 948 ms (58.89%), Space: 165 MB (57.77%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,41 +1,42 @@
 class Solution
 {
     public:
-        
-    bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
-    {
-        unordered_map<int,vector<int>> g;
 
-       	// g.resize(edges.size());
-        if (n == 1)
-            return true;
-        
-        for (auto x: edges)
+        bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
         {
-            g[x[0]].push_back(x[1]);
-            g[x[1]].push_back(x[0]);
-        }
+            vector<vector<int>>g(n+1);
 
-        bool vis[n];
-        memset(vis, false, sizeof(vis));
-        stack<int> s;
-        s.push(source);
-        vis[source] = true;
-        while (!s.empty())
-        {
-            int elem = s.top();
-            s.pop();
-            vis[elem] = true;
-            if(elem==destination )
-                return 1;
-            
-            for (auto x: g[elem])
+           	// g.resize(edges.size());
+            if (n == 1)
+                return true;
+
+            for (auto x: edges)
             {
-                
-                if (vis[x] == false)
-                    s.push(x);
+                g[x[0]].push_back(x[1]);
+                g[x[1]].push_back(x[0]);
             }
+
+            bool vis[n];
+            memset(vis, false, n);
+
+            stack<int> s;
+            s.push(source);
+
+            while (!s.empty())
+            {
+                int elem = s.top();
+                s.pop();
+                vis[elem] = true;
+                if (elem == destination)
+                    return 1;
+
+                for (auto x: g[elem])
+                {
+
+                    if (vis[x] == false)
+                        s.push(x);
+                }
+            }
+            return false;
         }
-        return false;
-    }
 };
