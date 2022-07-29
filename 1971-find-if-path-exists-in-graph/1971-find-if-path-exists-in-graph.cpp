aa739e117b53c07b8f97f3227a7df4d5562bd0a0
Prabal Jain <prabaljainn@gmail.com>
2022-07-30 00:41:36 +0530

    Time: 645 ms (92.39%), Space: 114.3 MB (95.93%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -1,47 +1,58 @@
-class Solution
-{
-    public:
-        vector<int> Rank;
-    vector<int> Parent;
+    static auto _______ =[]()
+    {
+       	// turn off sync
+        std::ios::sync_with_stdio(false);
+       	// untie in/out streams
+        std::cin.tie(nullptr);
+        return 0;
+    }();
 
-    int findParent(int x)
+    class Solution
     {
-        if (Parent[x] == x)
-            return x;
-        
-        return Parent[x] = findParent(Parent[x]);
-    }
-    
-    void Merge(int x, int y)
-    {
-        int xa = findParent(x);
-        int xb = findParent(y);
-        if(xa==xb)
-            return;
-        
-        if(Rank[xa] > Rank[xb])
+
+        public:
+
+            vector<int> Rank;
+        vector<int> Parent;
+
+        int findParent(int x)
         {
-            Rank[xa]+=Rank[xb];
+            if (Parent[x] == x)
+                return x;
+
+            return Parent[x] = findParent(Parent[x]);
+        }
+
+        void Merge(int x, int y)
+        {
+            int xa = findParent(x);
+            int xb = findParent(y);
+            if (xa == xb)
+                return;
+
+            if (Rank[xa] > Rank[xb])
+            {
+                Rank[xa] += Rank[xb];
                 Parent[findParent(y)] = xa;
-        }
-        else{
-            Rank[xb]+=Rank[xa];
-            Parent[findParent(x)] = xb;
-            
-        }
-    }
-    
-    bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
-    {
-        Parent.resize(n);
-        Rank.assign(n, 1);
-        iota(Parent.begin(),Parent.end(),0);
-   
-        for (const vector < int> &x : edges)
-        {
-            Merge(x[0], x[1]);
+            }
+            else
+            {
+                Rank[xb] += Rank[xa];
+                Parent[findParent(x)] = xb;
+            }
         }
 
-        return findParent(source) == findParent(destination);
-    }
-};
+        bool validPath(int n, vector<vector < int>> &edges, int source, int destination)
+        {
+            Parent.resize(n);
+            Rank.assign(n, 1);
+            iota(Parent.begin(), Parent.end(), 0);
+
+            for (const vector<int> &x: edges)
+            {
+                Merge(x[0], x[1]);
+            }
+
+            return findParent(source) == findParent(destination);
+        }
+    };
