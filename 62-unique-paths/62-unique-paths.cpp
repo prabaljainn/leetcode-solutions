2b33595541954d2458aa689ab7f82a0ea0a0ca03
Prabal Jain <prabaljainn@gmail.com>
2022-08-01 08:28:08 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (75.04%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -16,7 +16,10 @@ public:
         
     }
     int uniquePaths(int m, int n) {
-        memset(dp, -1, sizeof(dp));
+        
+        for(int i=0; i<=m; i++)
+            for(int j=0; j<=n; j++)
+                dp[i][j] = -1;
         
         return rec(m,n);
     }
