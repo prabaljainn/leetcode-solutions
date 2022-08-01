019a964ca6fbebdc1e50f056c3a054da9951533b
Prabal Jain <prabaljainn@gmail.com>
2022-08-01 08:24:39 +0530

    Time: 6 ms (12.58%), Space: 5.9 MB (90.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,40 +1,23 @@
-class Solution
-{
-    public:
-       	// at any point of time he can go right or down
-        int dp[105][105];
-    int m, n;
-    int rec(int i, int j)
-    {
-       	// pruning 
-        if (i >= m or j >= n)
+class Solution {
+public:
+    int dp[101][101];
+    
+    int rec(int m, int n){   // returns the number of ways you can reach 1,1
+        if(m<=0 or n<=0)
             return 0;
-       	// case
-        if (i == m - 1 and j == n - 1)
+        
+        if(m==1 and n==1)
             return 1;
-        int ans = 0;
-
-        if (dp[i][j] != -1)
-            return dp[i][j];
-
-        ans += rec(i + 1, j);
-        ans += rec(i, j + 1);
-
-        return dp[i][j] = ans;
+        
+        if(dp[m][n]!= -1)
+            return dp[m][n];
+        
+        return dp[m][n] = rec(m-1,n) + rec(m,n-1);
+        
     }
-
-    int uniquePaths(int M, int N)
-    {
-        m = M;
-        n = N;
-
-       	// manual memset
-        for (int i = 0; i < m + 1; i++)
-        {
-            for (int j = 0; j < n + 1; j++)
-                dp[i][j] = -1;
-        }
-
-        return rec(0, 0);
+    int uniquePaths(int m, int n) {
+        memset(dp, -1, sizeof(dp));
+        
+        return rec(m,n);
     }
 };
