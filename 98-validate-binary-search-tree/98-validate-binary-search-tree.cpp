d2e5f62bc3498c5729e092f38d4b7af7afe80f79
Prabal Jain <prabaljainn@gmail.com>
2022-08-03 02:09:10 +0530

    Time: 11 ms (92.61%), Space: 21.7 MB (30.71%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -1,29 +1,31 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
 
-class Solution {
-public:
-    bool v(TreeNode* root, long long l, long long r){
-        if(root==NULL)
-            return true;
-        
-        if(root->val <l or root->val > r or root->val==l or root->val==r)
-            return false;
-        
-        return v(root->left, l, root->val) and v(root->right, root->val, r);
-    }
-    
-    bool isValidBST(TreeNode* root) {
+class Solution
+{
+    public:
+        bool v(TreeNode *root, long long l, long long r)
+        {
+            if (root == NULL)
+                return true;
+
+            if (root->val < l or root->val > r or root->val == l or root->val == r)
+                return false;
+
+            return v(root->left, l, root->val) and v(root->right, root->val, r);
+        }
+
+    bool isValidBST(TreeNode *root)
+    {
         return v(root, -1e18, 1e18);
-        
     }
 };
