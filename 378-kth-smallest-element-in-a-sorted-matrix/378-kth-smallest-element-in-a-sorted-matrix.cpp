d36431e35cfcf0696480d41308595cea873640f0
Prabal Jain <prabaljainn@gmail.com>
2022-08-02 08:35:57 +0530

    Time: 42 ms (76.12%), Space: 13.1 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -7,45 +7,40 @@ class Solution
         int Row = n - 1;
         int Col = 0;
         int ans = 0;
-
-        while (Row>=0 and Col<n) //Termination condition) 
+        while (Row >= 0 and Col < n)	//Termination condition) 
         {
-            if(arr[Row][Col] <= mid){
-                ans+= (Row+1);
-                Col+=1;
+            if (arr[Row][Col] <= mid)
+            {
+                ans += (Row + 1);
+                Col += 1;
             }
-            else{
-                Row-=1;
+            else
+            {
+                Row -= 1;
             }
-            
         }
         return ans >= k;
-        }
+    }
 
-       	// check -> haveLessernumbers with sum<=k ->yes/no
-
-        int kthSmallest(vector<vector < int>> &arr, int k)
+    int kthSmallest(vector<vector < int>> &arr, int k)
+    {
+        n = arr.size();
+        int lo = arr[0][0];
+        int hi = arr[n - 1][n - 1];
+        int ans = lo;
+        while (hi >= lo)
         {
-            n = arr.size();
-            int lo = arr[0][0];
-            int hi = arr[n - 1][n - 1];
-            int ans = lo;
-            while (hi >= lo)
+            int mid = lo + (hi - lo) / 2;
+            if (check(arr, k, mid))
             {
-                int mid = lo + (hi - lo) / 2;
-                if (check(arr, k, mid))
-                {
-                    hi  = mid-1;
-                    
-                    ans = mid;
-                }
-                else
-                {
-                    lo = mid+1;
-                }
+                hi = mid - 1;
+                ans = mid;
             }
-            // cout<<check(arr,k,14);
-            
-            return ans;
+            else
+            {
+                lo = mid + 1;
+            }
         }
-    };
+        return ans;
+    }
+};
