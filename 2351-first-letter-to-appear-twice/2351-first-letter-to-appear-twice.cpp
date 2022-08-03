6161068db1ee1bbf63c3cfa351eaca0d7735c44b
Prabal Jain <prabaljainn@gmail.com>
2022-08-03 11:40:56 +0530

    Time: 0 ms (100.00%), Space: 6 MB (85.13%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        int arr[28];
+        memset(arr, 0, sizeof(arr));
+        
+        for(char x: s)
+        {
+            if(arr[x-'a']==1)
+                return x;
+            arr[x-'a']++;
+        }
+        return 'a';
+        
+    }
+};
