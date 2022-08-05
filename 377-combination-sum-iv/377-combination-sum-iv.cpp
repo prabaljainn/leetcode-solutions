f22325a19805bbe1395d2c331f8fd4922862af66
Prabal Jain <prabaljainn@gmail.com>
2022-08-05 09:45:27 +0530

    Time: 7 ms (19.94%), Space: 11.2 MB (5.01%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -1,7 +1,8 @@
 class Solution {
     int dp[1000100];
+    vector<int> arr;
     int n;
-    int rec(int level , vector<int> &arr){
+    int rec(int level ){
         if(level ==0 )
             return 1;
         
@@ -12,15 +13,17 @@ class Solution {
         int ans=0;
         for(int i=0 ; i<n; i++  ){
             if(level - arr[i] >= 0)
-            ans+=  rec(level - arr[i],arr);
+            ans+=  rec(level - arr[i]);
         }
         return dp[level] = ans;
     }
     
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        n  = nums.size();
+        n = nums.size();
+        arr.resize(nums.size());
+        arr = nums;
         memset(dp,-1,sizeof(dp));
-        return rec(target,nums);
+        return rec(target);
     }
 };
