7ba1296b346d23306e6d81ea0e0dd2a80c8ba9ba
Prabal Jain <prabaljainn@gmail.com>
2022-08-09 19:37:33 +0530

    Time: 120 ms (48.31%), Space: 9.3 MB (67.38%) - LeetHub

---
+++ 823-binary-trees-with-factors/823-binary-trees-with-factors.cpp
@@ -0,0 +1,53 @@
+class Solution
+{
+    public:
+    int mod  = 1e9+7;
+        vector<int> arr;
+    long long dp[100100];
+    long long n;
+    long long rec(int level)
+    {
+        if (level == 0)
+            return 1;
+
+        if (dp[level] != -1)
+            return dp[level];
+        
+        long long ans = 1;
+
+        long long i = 0;
+        long long j = level - 1;
+        while (i <= j)
+        {
+            long long mul = (1LL*arr[i] * arr[j]);
+            if ( mul == arr[level])
+            {
+                if (i == j)
+                    ans += (rec(i)*rec(i))%mod;
+                else
+                    ans += 2 *(rec(i) *rec(j))%mod;
+                i++;
+                j--;
+            }
+            else if (mul > arr[level])
+                j--;
+            else
+                i++;
+        }
+
+        return dp[level] = (int)(ans%mod);
+    }
+
+    int numFactoredBinaryTrees(vector<int> &arr)
+    {
+        sort(arr.begin(), arr.end());
+        n = arr.size();
+        this->arr = arr;
+        memset(dp, -1, sizeof(dp));
+        long long sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += rec(i);
+
+        return sum%mod;
+    }
+};
