c7eea6a2e1afb8826a2f6bb4f623862bc77526e6
Prabal Jain <prabaljainn@gmail.com>
2022-08-10 19:56:58 +0530

    Time: 59 ms (84.23%), Space: 24.1 MB (33.37%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.cpp
@@ -2,70 +2,28 @@ class Solution
 {
     public:
 
-        bool validPalindrome(string arr)
-        {
-            int i = 0;
-            int j = arr.size() - 1;
-            bool flag = false;
-
-            while (i <= j)
-            {
-                if (arr[i] == arr[j])
-                {
-                    i++;
-                    j--;
-                    continue;
-                }
-                else
-                {
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag == false)
-                return true;
-            flag = false;
-            int store1 = i;
-            int store2 = j;
-            i++;
-            
-            while (i <= j)
-            {
-                if (arr[i] == arr[j])
-                {
-                    i++;
-                    j--;
-                    continue;
-                }
-                else
-                {
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag == false)
-                return true;
-            flag = false;
-            i = store1;
-            j = store2-1;
-            while (i <= j)
-            {
-                if (arr[i] == arr[j])
-                {
-                    i++;
-                    j--;
-                    continue;
-                }
-                else
-                {
-                    flag = true;
-                    break;
-                }
-            }
-            if (flag == false)
-                return true;
-            return false;
-            
-            
+    bool palindrome(string s, int i , int j) {
+        while(i<j){
+            if(s[i] != s[j])
+                return false;
+            i++; j--;
         }
+        return true;
+    }
+    
+    bool validPalindrome(string s) {
+        
+        int i = 0;
+        int j = s.length()-1;
+        int cnt = 0;
+        
+        while(i<j){
+            if(s[i] != s[j]) {
+                return (palindrome(s,i+1,j)or(palindrome(s,i,j-1)));
+            }
+            
+            i++; j--;
+        }
+        return true;
+    }
 };
