862e0f8c9954deeee556859bdf6e41db7441ce3d
Prabal Jain <prabaljainn@gmail.com>
2022-09-10 16:50:16 +0530

    Time: 8 ms (68.03%), Space: 10.5 MB (37.98%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,17 +1,20 @@
-class Solution {
-public:
-    vector<int> intersection(vector<int>& arr1, vector<int>& arr2) {
-        unordered_set<int> s(arr1.begin(), arr1.end());
-        vector<int> ans;
-        ans.reserve(min(arr1.size(), arr2.size()));
-        
-        for(int x: arr2){
-            if(s.erase(x) > 0 )
+class Solution
+{
+    public:
+        vector<int> intersection(vector<int> &arr1, vector<int> &arr2)
+        {
+            unordered_set<int> s(arr1.begin(), arr1.end());
+            vector<int> ans;
+            ans.reserve(min(arr1.size(), arr2.size()));
+
+            for (int x: arr2)
             {
-                ans.push_back(x);
+                if (s.erase(x) > 0)
+                {
+                    ans.push_back(x);
+                }
             }
+
+            return ans;
         }
-        
-        return ans;
-    }
 };
