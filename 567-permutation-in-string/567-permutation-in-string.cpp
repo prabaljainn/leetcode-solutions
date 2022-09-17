03870911aa3433509ed5ceb8f6885230099b221c
Prabal Jain <prabaljainn@gmail.com>
2022-09-17 22:05:59 +0530

    Time: 2871 ms (5.00%), Space: 58.2 MB (6.51%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,37 +1,15 @@
-class Solution
-{
-    public:
-        bool checkInclusion(string s1, string s2)
-        {
-            if(s1.size()>s2.size())
-                return false;
-            int win_size = s1.size();
-            int n = s2.size();
-            vector<int> v(26, 0);
-            vector<int> v2(26, 0);
-
-            for (char x: s1)
-            {
-                v[x - 'a']++;
-            }
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(),s1.end());
+        int a=s1.size();
+        int n=s2.size();
+    for(int i=0;i<=n-a;i++){
+        string t=s2.substr(i,a);
+     sort(t.begin(),t.end());
+        if(t==s1)
             
-            
-            for (int i = 0; i < win_size; i++)
-            {
-                v2[s2[i] - 'a']++;
-            }
-            
-            if (v == v2)
-                return true;
-            
-            for (int i = win_size; i < n; i++)
-            {
-                v2[s2[i] - 'a']++;
-                v2[s2[i - win_size] - 'a']--;
-
-                if (v == v2)
-                    return true;
-            }
-            return false;
-        }
-};
+            return true;
+        
+    }
+        return false;}};
