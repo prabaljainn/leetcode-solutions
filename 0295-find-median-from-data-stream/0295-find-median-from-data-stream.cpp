cef231ca0fbe0550867f196bbaae3b7370099155
Prabal Jain <prabaljainn@gmail.com>
2022-10-11 23:03:41 +0530

    Time: 445 ms (76.88%), Space: 134.5 MB (6.24%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.cpp
@@ -1,82 +1,53 @@
 
-class DataDash {
-public:
-    int sum = 0;
-    int sumSq = 0;
-    int n = 0 ;
-    map<int, int> freq;
-    multiset<pair<int, int>> freqPair;
+
+class MedianFinder
+{
+    public: 
     multiset<int> left, right;
 
-    void balance() {
-        if(left.size() < right.size()) {
+    void balance()
+    {
+        if (left.size() < right.size())
+        {
             int x = *right.begin();
             left.insert(x);
             right.erase(right.find(x));
         }
-        if(left.size() > right.size() + 1) {
+        if (left.size() > right.size() + 1)
+        {
             int x = *left.rbegin();
             right.insert(x);
             left.erase(left.find(x));
         }
     }
-    void insert(int x) {
+    void addNum(int x)
+    {
 
-        //median
-        if(left.size() == 0)
+       	//median
+        if (left.size() == 0)
             left.insert(x);
-        else if(x > *left.rbegin()) {
+        else if (x > *left.rbegin())
+        {
             right.insert(x);
-        } else {
+        }
+        else
+        {
             left.insert(x);
         }
         balance();
-
-
-    }
-    void remove(int x) {
-
-        //median
-
-        if(left.find(x) != left.end())
-            left.erase(left.find(x));
-
-        else
-            right.erase(right.find(x));
-        balance();
     }
 
-    double median() {
-        if(left.size() == right.size())
+    double findMedian()
+    {
+        if (left.size() == right.size())
             return (double)(*left.rbegin() + *right.begin()) / 2;
-        else return
-               (double) *left.rbegin();
-    }
-
-   
-};
-
-
-
-class MedianFinder {
-public:DataDash d;
-    MedianFinder() {
-        
-    }
-    
-    void addNum(int num) {
-        d.insert(num);
-    }
-    
-    double findMedian() {
-        return d.median();
-        
+        else return (double) *left.rbegin();
     }
 };
 
 /**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
+ *Your MedianFinder object will be instantiated and called as such:
+ *MedianFinder* obj = new MedianFinder();
+ *obj->addNum(num);
+ *double param_2 = obj->findMedian();
  */
