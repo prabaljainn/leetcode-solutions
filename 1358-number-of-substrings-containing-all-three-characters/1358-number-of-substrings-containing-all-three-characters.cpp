5576a709f8a4f44d88a01754998ae29bffddbedd
Prabal Jain <prabaljainn@gmail.com>
2023-02-08 20:31:05 +0530

    Time: 129 ms (15.50%), Space: 18.6 MB (5.87%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,49 +1,36 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int n= s.size();
-        // int tot = (n*(n+1))/2;
-        // tot--;
-        int head = -1; 
-        int tail = 0;
-        int val =0;
-        map<char, int> mp;
-        mp['a']=0;
-        mp['b']=0;
-        mp['c']=0;
-        while(tail<n){
+        int head=-1;
+        int tail=0;
+        int ans=0;
+        map<char,int>mp;
+        while(tail<s.length())
+        {   
             
-            while(
-                (head+1<n) and (((mp['a']+(s[head+1]=='a') <1 )or
-                (mp['b']+(s[head+1]=='b') <1) or
-                (mp['c']+(s[head+1]=='c') <1))
-            
-            )
-                 )
+            while(head+1<s.length() and mp.size()<3)
             {
-                head++; 
+                head++;
                 mp[s[head]]++;
+                
             }
-            
-            val+= (n-head-1);
-            cout<<val<<endl;
-            
-            if(tail<= head)
-            {
-                // if(mp[s[tail]]>0)
+        
+            if(mp['a'] and mp['b'] and mp['c'])
+             ans+=(s.length()-head);
+
+            if(head>=tail)
+            {   
                 mp[s[tail]]--;
+                if(mp[s[tail]]==0)mp.erase(s[tail]);
                 tail++;
             }
             else{
-                // if(mp[s[tail]]>0)
                 mp[s[tail]]--;
+                if(mp[s[tail]]==0)mp.erase(s[tail]);
                 tail++;
-                head= tail-1;
+                head=tail-1;
             }
-            
         }
-        // cout<<(tot)<<" "<<val;
-        return val;
-        
+        return ans;
     }
 };
